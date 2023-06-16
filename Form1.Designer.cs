35ca85692449bf674d94c478cfc7d1f9b23aa57c
Uğur Özşahin <ozsahinqreus@gmail.com>
2023-06-16 20:34:47 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace PhotoGrouper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(310, 410);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(310, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EXCEL DOSYASI SEÇ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(878, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(379, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "FOTOĞRAF KLASÖRÜNÜ SEÇ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(878, 41);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(379, 410);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            this.openFileDialog2.Multiselect = true;
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(693, 474);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "KAYDET";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(368, 474);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(148, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Orjinale Dön";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(368, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(453, 410);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown_1);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove_1);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(522, 474);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(148, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Kes   (CTRL+X)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 612);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using System.Drawing;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using ExcelApp = Microsoft.Office.Interop.Excel;
+
+using gfoidl.Imaging;
+using System;
+
+namespace PhotoGrouper
+{
+    public partial class Form1 : Form
+
+    {
+
+
+        int currentcell = 0;
+        int nextcell = 0;
+        string yol = "C:/Users/UGUR/Desktop/bilgisayar organizasyonu";
+        string DosyaYolu;
+        string DosyaAdi;
+        DataTable dt;
+        //
+        private Image _originalImage;
+        private bool _selecting;
+        private Rectangle _selection;
+
+        public DataTable ToDataTable(ExcelApp.Range range, int rows, int cols)
+        {
+            DataTable table = new DataTable();
+            for (int i = 1; i <= rows; i++)
+            {
+                if (i == 1)
+                { // ilk satırı Sutun Adları olarak kullanıldığından
+                  // bunları Sutün Adları Olarak Kaydediyoruz.
+                    for (int j = 1; j <= cols; j++)
+                    {
+                        //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                        if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                            table.Columns.Add(range.Cells[i, j].Value2.ToString());
+                        else //Boş olduğunda Kaçınsı Sutünsa Adı veriliyor.
+                            table.Columns.Add(j.ToString() + ".Sütun");
+                    }
+                    continue;
+                }
+                //Yukarıda Sütunlar eklendi
+                // onun şemasına göre yeni bir satır oluşturuyoruz. 
+                //Okunan verileri yan yana sıralamak için
+                var yeniSatir = table.NewRow();
+                for (int j = 1; j <= cols; j++)
+                {
+                    //Sütunların içeriği boş mu kontrolü yapılmaktadır.
+                    if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                        yeniSatir[j - 1] = range.Cells[i, j].Value2.ToString();
+                    else // İçeriği boş hücrede hata vermesini önlemek için
+                        yeniSatir[j - 1] = String.Empty;
+                }
+                table.Rows.Add(yeniSatir);
+            }
+            return table;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            _originalImage = pictureBox1.Image.Clone() as Image;
+
+        }
+       
+       
+        //---------------------------------------------------------------------
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası | *.xls; *.xlsx; *.xlsm";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                DosyaYolu = file.FileName;// seçilen dosyanın tüm yolunu verir
+                DosyaAdi = file.SafeFileName;// seçilen dosyanın adını verir.
+                ExcelApp.Application excelApp = new ExcelApp.Application();
+                if (excelApp == null)
+                { //Excel Yüklümü Kontrolü Yapılmaktadır.
+                    MessageBox.Show("Excel yüklü değil.");
+                    return;
+                }
+                //Excel Dosyası Açılıyor.
+                ExcelApp.Workbook excelBook = excelApp.Workbooks.Open(DosyaYolu);
+                //Excel Dosyasının Sayfası Seçilir.
+                ExcelApp._Worksheet excelSheet = excelBook.Sheets[1];
+                //Excel Dosyasının ne kadar satır ve sütun kaplıyorsa tüm alanları alır.
+                ExcelApp.Range excelRange = excelSheet.UsedRange;
+                int satirSayisi = excelRange.Rows.Count; //Sayfanın satır sayısını alır.
+                int sutunSayisi = excelRange.Columns.Count;//Sayfanın sütun sayısını alır.
+                dt = ToDataTable(excelRange, satirSayisi, sutunSayisi);
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+                //Okuduktan Sonra Excel Uygulamasını Kapatıyoruz.
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+            else
+            {
+                MessageBox.Show("Dosya Seçilemedi.");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string oge in openFileDialog2.FileNames)
+                {
+                    listView1.Items.Add(System.IO.Path.GetFileName(oge));
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var item in dataGridView1.Rows)
+            {
+                File.Copy(listView1.Items[0].ToString(), "C:/Users/UGUR/Desktop/asdsadadas" + dataGridView1.CurrentRow.Selected);
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = _originalImage.Clone() as Image;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    Image img = pictureBox1.Image;
+                    Bitmap bmp = new Bitmap(img.Width, img.Height);
+                    Graphics gra = Graphics.FromImage(bmp);
+                    gra.DrawImageUnscaled(img, new Point(0, 0));
+                    gra.Dispose();
+
+                    string deger = dataGridView1.SelectedCells[0].Value.ToString();
+                    string ogrisim = "\\"+deger+".jpg";
+                    currentcell = dataGridView1.CurrentCell.RowIndex;
+                    if (currentcell!=0)
+                    {
+                        int iColumn = dataGridView1.CurrentCell.ColumnIndex;
+                        int iRow = dataGridView1.CurrentCell.RowIndex;
+                        if (iColumn == dataGridView1.Columns.Count - 1)
+                            dataGridView1.CurrentCell = dataGridView1[0, iRow + 1];
+                        else
+                            dataGridView1.CurrentCell = dataGridView1[iColumn, iRow+1];
+
+                    }
+                    
+
+                string belgelerim = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    bmp.Save(belgelerim + ogrisim, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    bmp.Dispose();
+                    
+                    int next = Convert.ToInt32(listView1.FocusedItem.Index) + 1;
+                    
+                    listView1.Items[next].Selected = true;
+
+                    degisken = secilen.SubItems[sayac].Text;
+                   
+                    // focus değiştirmede kaldım focus değişecek yol değişecek aynı foto kaydedilmeyecek
+                  
+
+                }
+            }
+            catch { }
+        }
+        ListViewItem secilen; 
+        string degisken;
+        int sayac = 0;
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             secilen = listView1.FocusedItem;//focusedıtem senin seçtiğin listview itemi alır
+            
+             degisken = secilen.SubItems[sayac].Text;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+           
+
+            string alan = degisken;
+            pictureBox1.ImageLocation = yol + "/" + alan;
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            // Seçimin başlangıç ​​noktası:
+            if (e.Button == MouseButtons.Left)
+            {
+                
+                _selecting = true;
+                _selection = new Rectangle(new Point(e.X, e.Y), new Size());
+            }
+
+        }
+
+        private void pictureBox1_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            // Seçimin gerçek boyutunu güncelleyin:
+            if (_selecting)
+            {
+                _selection.Width = e.X - _selection.X;
+                _selection.Height = e.Y - _selection.Y;
+
+                // Resim kutusunu yeniden çizin:
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp_1(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && _selecting)
+            {
+               
+
+                _selection.Width = e.X - _selection.X;
+                _selection.Height = e.Y - _selection.Y;
+                _selecting = false;
+                
+
+            }
+          
+
+
+
+        }
+
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+            if (_selecting)
+            {
+                //Geçerli seçimi gösteren bir dikdörtgen çizin
+                Pen pen = Pens.GreenYellow;
+                e.Graphics.DrawRectangle(pen, _selection);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Image img = pictureBox1.Image.Crop(_selection);
+            pictureBox1.Image = img.Fit2PictureBox(pictureBox1);
+            _selection.Width = 0;
+            _selection.Height = 0;
+
+            
+            
+              
+            
+
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+       
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
+
